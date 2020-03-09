75a1d1da48d429a52814026a00457e439e30b48b
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-03-09 03:10:01 +0300

    Update f_code.py

--- f_code.py
+++ f_code.py
@@ -1,17 +1,34 @@
 from vpython import *
 vc = vector
 
-class Cubie:
-    def __init__(self):
-        pass
+def make_side(pos_s=[vec(0,0,0) for _ in range(4)], col=color.white):
+        pos_s = [vec(*i) for i in pos_s]
+        pts = [
+            vertex(pos=pos_s[0], color=col),
+            vertex(pos=pos_s[1], color=col),
+            vertex(pos=pos_s[2], color=col),
+            vertex(pos=pos_s[3], color=col),
+        ]
+        return quad(vs=pts)
+
+def make_cubie(box, col):
+    sides = [
+        make_side([(0,0,0), (1,0,0), (1,1,0), (0,1,0)], col[0]),
+        make_side([(0,0,0), (0,0,1), (0,1,1), (0,1,0)], col[2]),
+        make_side([(0,0,0), (1,0,0), (1,0,1), (0,0,1)], col[4]),
+
+        make_side([(0,0,1), (1,0,1), (1,1,1), (0,1,1)], col[1]),
+        make_side([(1,0,0), (1,0,1), (1,1,1), (1,1,0)], col[3]),
+        make_side([(0,1,0), (1,1,0), (1,1,1), (0,1,1)], col[5]),
+    ]
+    return compound([side for side in sides])
+
+cu = make_cubie(1, [color.red, color.blue, color.green, color.purple, color.yellow, color.cyan])
+
+while 1:
+    cu.pos.x+=0.000001
 
 #compound
-
-a = vertex( pos=vec(0,0,0), color=color.blue)
-b = vertex( pos=vec(1,0,0), color=color.red)
-c = vertex( pos=vec(1,1,0), color=color.red)
-d = vertex( pos=vec(0,1,0), color=color.red)
-Q = quad( vs=[a,b,c,d])
 
 #arrow(pos=b.pos, axis=vc(1,0,0))
 
