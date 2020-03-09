4ead16872c0580a8448a9bc4946f223e189dd8e1
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-03-09 03:12:01 +0300

    .

--- f_code.py
+++ f_code.py
@@ -28,12 +28,7 @@ cu = make_cubie(1, [color.red, color.blue, color.green, color.purple, color.yell
 while 1:
     cu.pos.x+=0.000001
 
-#compound
-
 #arrow(pos=b.pos, axis=vc(1,0,0))
-
-#redbox=box(pos=vc(4,2,3),
-#           size=vc(8,4,6),color=color.red)
 
 '''list = [box(pos=vc(i, j, k), size=vc(0.75, 0.75, 0.75)) for k in range(3) for j in range(3) for i in range(3) if i!=1 or j!=1 or k!=1]
 
