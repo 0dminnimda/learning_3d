9709ad9068e59519409a694bc514cc386ec7d424
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-03-09 03:53:57 +0300

    Update f_code.py

--- f_code.py
+++ f_code.py
@@ -1,8 +1,7 @@
 from vpython import *
 vc = vector
 
-def make_side(pos_s=[vec(0,0,0) for _ in range(4)], col=color.white):
-        pos_s = [vec(*i) for i in pos_s]
+def make_side(pos_s=[vec(0, 0, 0) for _ in range(4)], col=color.white):
         pts = [
             vertex(pos=pos_s[0], color=col),
             vertex(pos=pos_s[1], color=col),
@@ -11,29 +10,41 @@ def make_side(pos_s=[vec(0,0,0) for _ in range(4)], col=color.white):
         ]
         return quad(vs=pts)
 
-def make_cubie(box, col):
+def make_cubelet(col, pos=vec(0, 0, 0), size=1):
+    # set points of sides
     sides = [
-        make_side([(0,0,0), (1,0,0), (1,1,0), (0,1,0)], col[0]),
-        make_side([(0,0,0), (0,0,1), (0,1,1), (0,1,0)], col[2]),
-        make_side([(0,0,0), (1,0,0), (1,0,1), (0,0,1)], col[4]),
+        [[0, 0, 0], [size, 0, 0], [size, size, 0], [0, size, 0]],
+        [[0, 0, 0], [0, 0, size], [0, size, size], [0, size, 0]],
+        [[0, 0, 0], [size, 0, 0], [size, 0, size], [0, 0, size]],
 
-        make_side([(0,0,1), (1,0,1), (1,1,1), (0,1,1)], col[1]),
-        make_side([(1,0,0), (1,0,1), (1,1,1), (1,1,0)], col[3]),
-        make_side([(0,1,0), (1,1,0), (1,1,1), (0,1,1)], col[5]),
+        [[0, 0, size], [size, 0, size], [size, size, size], [0, size, size]],
+        [[size, 0, 0], [size, 0, size], [size, size, size], [size, size, 0]],
+        [[0, size, 0], [size, size, 0], [size, size, size], [0, size, size]],
     ]
+
+    # turn points to "vec" and add start point
+    for s in range(len(sides)):
+        for p in range(len(sides[s])):
+            sides[s][p] = vec(*sides[s][p]) + pos
+
+    # make list of sides
+    for s in range(len(sides)):
+        sides[s] = make_side(sides[s], col[s])
+    
+    # compound all sides to one object
     return compound([side for side in sides])
 
-cu = make_cubie(1, [color.red, color.blue, color.green, color.purple, color.yellow, color.cyan])
+class Cube:
+    def __init__(self):
+        list = [make_cubelet([color.red, color.blue, color.green, color.purple, color.yellow, color.cyan], pos=vec(i, j, k), size=0.75)
+                for k in range(3) for j in range(3) for i in range(3)
+                if i!=1 or j!=1 or k!=1]
+        pass
 
-while 1:
-    cu.pos.x+=0.000001
+#cu = make_cubelet([color.red, color.blue, color.green, color.purple, color.yellow, color.cyan])
+cu = Cube()
 
-#arrow(pos=b.pos, axis=vc(1,0,0))
+#while 1:
+#    cu.pos.x+=0.000002
 
-'''list = [box(pos=vc(i, j, k), size=vc(0.75, 0.75, 0.75)) for k in range(3) for j in range(3) for i in range(3) if i!=1 or j!=1 or k!=1]
-
-for b in list:
-    print(b.pos)
-
-while 1:
-    pass'''
+#arrow(pos=b.pos, axis=vc(1, 0, 0))
